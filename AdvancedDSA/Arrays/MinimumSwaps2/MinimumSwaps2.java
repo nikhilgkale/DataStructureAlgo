258664301f44fa20afcd31986036834ef860baa1
nikhilgkale <nikhilgkale@gmail.com>
2022-12-22 19:00:25 +0530

    Minimum Swap2 Problem

---
+++ AdvancedDSA/Arrays/MinimumSwaps2/MinimumSwaps2.java
@@ -0,0 +1,22 @@
+package AdvancedDSA.Arrays.MinimumSwaps2;
+
+public class MinimumSwaps2 {
+    public static void main(String[] args) {
+        System.out.println(solve(new int[]{1,2,3,4,0}));
+    }
+    public static int solve(int[] A) {
+        int count=0;
+        int N=A.length;
+        int s=0;
+        while(s<N){
+            if(A[s]!=s){
+                count++;
+                int temp=A[A[s]];
+                A[A[s]]=A[s];
+                A[s]=temp;
+            }
+            else s++;
+        }
+        return count;
+    }
+}
